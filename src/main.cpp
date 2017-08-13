a53cf1b62059bb4d0551cbd55c7e0d37bbd3ff69
Spidersbox <Spider@Spidersbox.com>
2017-08-13 12:55:20 -0700

    added disconnection of non bcf peers

--- src/main.cpp
+++ src/main.cpp
@@ -2960,7 +2960,18 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         if (!vRecv.empty())
             vRecv >> addrFrom >> nNonce;
         if (!vRecv.empty())
-            vRecv >> pfrom->strSubVer;
+        {
+          vRecv >> pfrom->strSubVer;
+          printf("peer connecting subver is %s",pfrom->strSubVer.c_str());
+          int iSubVer=pfrom->strSubVer.find("BitcoinFast");
+          if(iSubVer < 1)
+          {
+            printf("  -  disconnecting .....\n");
+            pfrom->fDisconnect = true;
+            return false;
+          }
+          printf("\n");
+        }
         if (!vRecv.empty())
             vRecv >> pfrom->nStartingHeight;
 
